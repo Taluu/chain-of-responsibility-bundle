a8603eafc7e6c581bbb2ac2e287850162d0acd49
Baptiste Clavié <clavie.b@gmail.com>
2017-10-29 18:26:23 +0100

    Remove the handle() from LinkInterface
    The link interface should really just ensure that the object can
    receive a successor through a `setSuccessor` call.
    
    How the data is passed should be the implementation's concern,
    not the contract's.

--- src/LinkInterface.php
+++ src/LinkInterface.php
@@ -3,6 +3,5 @@ namespace ChainOfResponsibilityBundle;
 
 interface LinkInterface
 {
-    public function handle(DataInterface $data): void;
     public function setSuccessor(?self $successor): void;
 }
